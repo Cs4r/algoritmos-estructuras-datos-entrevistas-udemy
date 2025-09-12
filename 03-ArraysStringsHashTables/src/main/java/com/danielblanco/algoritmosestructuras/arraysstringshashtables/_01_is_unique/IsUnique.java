acacaa289866a0bf0d90437cad24fd9c6775055a
César Aguilera <cesar.aguilera.p@gmail.com>
2025-09-12 20:58:51 +0200

    Exercise 1

--- 03-ArraysStringsHashTables/src/main/java/com/danielblanco/algoritmosestructuras/arraysstringshashtables/_01_is_unique/IsUnique.java
+++ 03-ArraysStringsHashTables/src/main/java/com/danielblanco/algoritmosestructuras/arraysstringshashtables/_01_is_unique/IsUnique.java
@@ -1,5 +1,10 @@
 package com.danielblanco.algoritmosestructuras.arraysstringshashtables._01_is_unique;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 /*
  * Dado un método que recibe una String, comprobar si todos los caracteres son únicos o no.
  *
@@ -8,7 +13,18 @@ package com.danielblanco.algoritmosestructuras.arraysstringshashtables._01_is_un
  */
 public class IsUnique {
 
-  public boolean isUnique(String s) {
-    throw new UnsupportedOperationException("Not implemented yet");
-  }
+    public boolean isUnique(String s) {
+        Set<Character> elements = new HashSet<>();
+
+        for (var c : s.toCharArray()) {
+
+            if (elements.contains(c)) {
+                return false;
+            }
+
+            elements.add(c);
+        }
+
+        return true;
+    }
 }
