14366f1baa388338e22d734ad64120633e6b692e
César Aguilera <cesar.aguilera.p@gmail.com>
2025-11-01 11:58:55 +0100

    solution to anagrams

--- 03-ArraysStringsHashTables/src/main/java/com/danielblanco/algoritmosestructuras/arraysstringshashtables/_03_group_anagrams/GroupAnagrams.java
+++ 03-ArraysStringsHashTables/src/main/java/com/danielblanco/algoritmosestructuras/arraysstringshashtables/_03_group_anagrams/GroupAnagrams.java
@@ -1,6 +1,6 @@
 package com.danielblanco.algoritmosestructuras.arraysstringshashtables._03_group_anagrams;
 
-import java.util.List;
+import java.util.*;
 
 /*
  * Un anagrama es una palabra creada a partir de la reordenación de las letras de otra palabra. Ej: saco - caso
@@ -12,7 +12,46 @@ import java.util.List;
  */
 public class GroupAnagrams {
 
-  public List<List<String>> groupAnagrams(String[] words) {
-    throw new UnsupportedOperationException("Not implemented yet");
-  }
+    public List<List<String>> groupAnagrams(String[] words) {
+        Map<Map<Character, Integer>, List<String>> freqToWords = new HashMap<>();
+
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+            Map<Character, Integer> frequencies = toFrequencies(word);
+
+            if (freqToWords.containsKey(frequencies)) {
+                freqToWords.get(frequencies).add(word);
+            } else {
+                List<String> list = new ArrayList<>();
+                list.add(word);
+                freqToWords.put(frequencies, list);
+            }
+        }
+
+        List<List<String>> output = new ArrayList<>();
+
+        for (var entry : freqToWords.entrySet()) {
+            output.add(entry.getValue());
+        }
+
+        return output;
+    }
+
+
+    private Map<Character, Integer> toFrequencies(String word) {
+        Map<Character, Integer> frequencies = new HashMap<>();
+
+        for (int i = 0; i < word.length(); i++) {
+            char letter = word.charAt(i);
+
+            if (frequencies.containsKey(letter)) {
+                frequencies.put(letter, frequencies.get(letter) + 1);
+            } else {
+                frequencies.put(letter, 1);
+            }
+        }
+
+        return frequencies;
+    }
+
 }
